df7ab3d99bdb8d94c22c2fd56d55c97cbc5899c3
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2020-12-03 08:17:58 +0200

    Update TicTacToe.java

--- src/test/tictactoe/TicTacToe.java
+++ src/test/tictactoe/TicTacToe.java
@@ -1,40 +1,40 @@
 package test.tictactoe;
 
-import java.awt.image.AreaAveragingScaleFilter;
 import java.io.*;
-import java.util.*;
 
-public class TicTacToe implements Serializable{
+public class TicTacToe{
 
-
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        System.out.println("Выберите пункт меню: \n1 Играть \n2 История \n3 Выход");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Select menu item: \n1 Play \n2 History \n3 Exit");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String menu = reader.readLine();
+        String again = "";
         Game game = new Game();
+        FileInputStream fin = new FileInputStream("notes.txt");
+
 
         if(menu.equals("1")){
-            System.out.println("Введите имена игроков");
+            System.out.println("Enter players names");
             String xPlayer = reader.readLine();
             String oPlayer = reader.readLine();
             if(!xPlayer.isEmpty() && !oPlayer.isEmpty()){
                 game.game(xPlayer, oPlayer);
+
+                System.out.println("Again? y/n");
+                again = reader.readLine();
+                if(again.equalsIgnoreCase("y")){
+                    game.game(xPlayer, oPlayer);
+                }
+                else if(again.equalsIgnoreCase("n")){
+                    System.exit(0);
+                }
             }
         }
+
         else if(menu.equals("2")){
-            ArrayList<History> list = new ArrayList<>();
-            try(ObjectInputStream is = new ObjectInputStream(new FileInputStream("tictactoe.txt"))){
-                list = (ArrayList<History>) is.readObject();
-            }catch(Exception ex){
-
-                System.out.println(ex.getMessage());
-            }
-
-
-            for(History h : list){
-                System.out.println(h.getPl1() + " " + h.getPl2());
-                System.out.println(h.getTable().toString());
+            int i=-1;
+            while((i=fin.read())!=-1){
+                System.out.print((char)i);
             }
         }
 
@@ -42,10 +42,6 @@ public class TicTacToe implements Serializable{
             System.exit(0);
         }
         else{
-            System.out.println("Пункт меню указан неверно. Запустите приложение заново");
+            System.out.println("Restart application");
         }
-
     }
-
-
-}
